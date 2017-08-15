05a8fc699d23f8e607e0b9f583c7c8a7684837aa
Curtis Beach <curtisbeach@curtiss-macbook-air-2.local>
2017-08-15 13:56:18 -0700

    Initial Commit

---
+++ LearningXCode/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LearningXCode
+//
+//  Created by Curtis Beach on 8/15/17.
+//  Copyright © 2017 Curtis Beach. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
